cf50839d2d4343a12ed664b080810efacdb9864d
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-09 19:16:00 +0200

    [ADDED] Chats routes and controller - show all chats is working - create chat is working

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :applications
+  resources :applications do 
+    resources :chats
+  end
 end
