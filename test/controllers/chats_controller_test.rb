9f82acdbcae20f72e2f50a6b0ff5c2e4d985e5ac
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-12 21:10:17 +0200

    [ADDED] chat controller tests

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+  test "Should get index" do
+    get application_chats_url(application_id: @applicationOne.token)
+    assert_response :success 
+    assert_equal Chat.where(application_id: @applicationOne.id).all.as_json(:except => [:id, :application_id]), JSON.parse(response.body)
+  end
+  test "Should create chat" do
+    post application_chats_url(application_id: @applicationOne.token)
+    assert_response :success 
+    assert_not_nil JSON.parse(response.body)['number']
+  end
 end
