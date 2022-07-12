a4495983a9a2f9e205b582fecd1a6d9aaae7b1bd
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-12 17:26:45 +0200

    [ADDED] application model tests for uniquenes and name presenece

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+  test "Should not save application without name" do
+    application = Application.new
+    assert_not application.save, "Saved the application without a name"
+  end
+  test "Should not save duplicate token in aplication" do
+    application = Application.new(token: @applicationOne.token, name: @applicationOne.name)
+    assert_not application.save, "Saved the application with duplicate token"
+  end
 end
