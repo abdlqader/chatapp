2d1777a4e6acc8539b377e1564878a5622d13dfe
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-12 17:26:00 +0200

    [FIX] application name must be presence

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
+    validates :name, presence: true
     validates_uniqueness_of :token
     attribute :chats_count, :integer, default: 0
     has_many :chats, dependent: :delete_all
