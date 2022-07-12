70359b0d8f791f28bf04ba1235fff6df5ee1927d
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-12 21:13:34 +0200

    [ADDED] message controller tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+  test "Should get index" do
+    get application_chat_messages_url(application_id: @applicationOne.token,chat_id: @chatOne.number)
+    assert_response :success 
+    assert_equal Message.where(chat_id: @chatOne.id).as_json(:except => [:id, :chat_id]), JSON.parse(response.body)
+  end
+  test "Should create message" do
+    post application_chat_messages_url(application_id: @applicationOne.token,chat_id: @chatOne.number)
+    assert_response :success 
+    assert_not_nil JSON.parse(response.body)['number']
+  end
 end
