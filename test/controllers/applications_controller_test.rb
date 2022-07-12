c898c8813b52958c79a62f736cee9d2da1740f51
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-12 20:32:58 +0200

    [ADDED] application controller test

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
-
 class ApplicationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+  test "Should get index" do
+    get applications_url
+    assert_response :success 
+    assert_equal Application.all.as_json(:except => :id), JSON.parse(response.body)
+  end
+  test "Should show application" do
+    get application_url(@applicationOne.token)
+    assert_response :success 
+    assert_equal @applicationOne.as_json(:except => :id), JSON.parse(response.body)
+  end
+  test "Should create application" do
+    post applications_url, params: { name: 'test'}
+    assert_response :success 
+    assert_equal 'test', JSON.parse(response.body)['name']
+    assert_not_nil JSON.parse(response.body)['token']
+  end
 end
